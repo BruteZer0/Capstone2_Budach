e63bfb9b212db8dede2a6cac243ec847a3b0eb0c
BruteZer0 <uybudach08@gmail.com>
2026-05-27 12:24:59 -0400

    Topping POJO properties/constructor/getter commit

--- src/main/java/org/example/Topping.java
+++ src/main/java/org/example/Topping.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public class Topping {
+    private String name;
+    private ToppingCategory category;
+    private BigDecimal pricePerUnit;
+
+    public Topping(String name, ToppingCategory category, BigDecimal pricePerUnit) {
+        this.name = name;
+        this.category = category;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ToppingCategory getCategory() {
+        return category;
+    }
+
+    public BigDecimal getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
