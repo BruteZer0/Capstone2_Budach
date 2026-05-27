e87dd3e5736125522cb7b4fa8de8b88fd6a6f670
BruteZer0 <uybudach08@gmail.com>
2026-05-27 12:18:01 -0400

    Pizza properties commit

--- src/main/java/org/example/Pizza.java
+++ src/main/java/org/example/Pizza.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class Pizza {
+    private PizzaSize size;
+    private CrustType type;
+    private boolean stuffedCrust;
+    private String sauce;
+    private boolean isSignature;
+    private String signatureName;
+
+
 }
