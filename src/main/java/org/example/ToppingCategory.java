3a8bc9c0daa916b55d5871283b1fbb9f11ed5aad
BruteZer0 <uybudach08@gmail.com>
2026-05-27 13:44:48 -0400

    ToppingCategory enum class commit

--- src/main/java/org/example/ToppingCategory.java
+++ src/main/java/org/example/ToppingCategory.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 
 public enum ToppingCategory {
     //PIZZA SIZE:                   PERSONAL                           MEDIUM                    LARGE
