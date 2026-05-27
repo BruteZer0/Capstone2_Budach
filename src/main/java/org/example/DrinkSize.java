bbb52a59a2baeb98fb9724cef9ea1eb434971cb7
BruteZer0 <uybudach08@gmail.com>
2026-05-27 12:52:30 -0400

    Pizza/drink size enum class commit

--- src/main/java/org/example/DrinkSize.java
+++ src/main/java/org/example/DrinkSize.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public enum DrinkSize {
+    SMALL("Small", new BigDecimal("2.00")),
+    MEDIUM("Medium", new BigDecimal("2.50")),
+    LARGE("Large", new BigDecimal("3.00"));
+
+    private final String displayName;
+    private final BigDecimal price;
+
+    DrinkSize(String displayName, BigDecimal price) {
+        this.displayName = displayName;
+        this.price = price;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
