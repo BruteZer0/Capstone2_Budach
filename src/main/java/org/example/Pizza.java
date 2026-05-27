faf00cd886afa58c3c2df1b5ba84bbd23d690720
BruteZer0 <uybudach08@gmail.com>
2026-05-27 13:55:01 -0400

    organization package commit

--- src/main/java/org/example/Pizza.java
+++ src/main/java/org/example/Pizza.java
@@ -1,6 +1,13 @@
 package org.example;
 
+import org.example.enums.CrustType;
+import org.example.enums.PizzaSize;
+
+import java.math.BigDecimal;
+
 public class Pizza {
+    private static final BigDecimal STUFF_CRUST_PRICE = new BigDecimal("2.00");
+
     private PizzaSize size;
     private CrustType type;
     private boolean stuffedCrust;
